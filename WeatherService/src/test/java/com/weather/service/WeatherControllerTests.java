6c603b8579ed12c819f14d733e4c9a13cf3821d9
BUKOLA OYELADE <BUKOLA OYELADE>
2016-10-04 09:44:39 +0100

    Remove target directory

--- WeatherService/src/test/java/com/weather/service/WeatherControllerTests.java
+++ WeatherService/src/test/java/com/weather/service/WeatherControllerTests.java
@@ -39,7 +39,9 @@ public class WeatherControllerTests {
    @Test
     public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
 
-        this.mockMvc.perform(get("/weather")).andDo(print()).andExpect(status().isOk())
+        this.mockMvc.perform(get("/weather"))
+                .andDo(print())
+                .andExpect(status().isOk())
                 .andExpect(jsonPath("$.tempCelsius").value(40));
 
     }
